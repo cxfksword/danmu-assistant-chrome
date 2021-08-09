920ce6730e44a38a1f6e393307daa90968404822
cxfksword <cxfksword@gmail.com>
2021-08-10 01:45:58 +0800

    优化build处理

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { resolve } from "path";
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import copy from 'rollup-plugin-copy'
+import glob from 'fast-glob'
+import packageJson from "./package.json";
+import manifestJson from "./src/manifest.json";
+import fs from 'fs-extra'
+ 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: resolve(__dirname, "src"),
-
+  
   build: {
+      emptyOutDir: true,
       outDir: resolve(__dirname, 'dist'),
       rollupOptions: {
           input: {
@@ -17,11 +23,31 @@ export default defineConfig({
   },
   plugins: [
       vue(),
-      copy({
-        targets: [
-          { src: 'src/manifest.json', dest: resolve(__dirname, 'dist/') },
-          { src: 'src/assets/icon/**/*', dest: resolve(__dirname, 'dist/assets/icon') }
-        ]
-      })
+      {
+        name: 'output_manifest',
+        generateBundle(outputOptions, bundle) {
+          manifestJson.version = packageJson.version;
+          this.emitFile({
+              type: 'asset',
+              fileName: 'manifest.json',
+              source: JSON.stringify(manifestJson, null, 4) 
+          });
+        }
+      },
+      {
+        name: 'output_icon_files',
+        async generateBundle() {
+          const src =  'src/assets/icon/';
+          const dest = 'assets/icon/';
+          const files = await glob('**/*', { cwd: 'src/assets/icon/' })
+    
+          await Promise.all(files.map(file => (async () => this.emitFile({
+            type: 'asset',
+            fileName: path.join(dest, file),
+            source: await fs.readFile(path.join(src, file))
+          }))()))
+        }
+      }
+      
   ],
 })
